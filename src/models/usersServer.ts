b8ea7339ddd7e2a923d72d03f28200d5c2d4b8d5
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2024-04-25 23:28:00 +0900

    유저 스키마 생성

---
+++ src/models/usersServer.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+interface DBUser {
+  nickname: string;
+  email: string;
+  password: string;
+  phone_number: string;
+  birth_day: Date;
+  profile_image: string;
+}
+
+const UsersSchema = new Schema<DBUser>(
+  {
+    nickname: { type: String, maxlength: 50 },
+    email: {
+      type: String,
+      unique: true,
+      maxlength: 200,
+      trim: true,
+      required: true,
+    },
+    password: { type: String, maxlength: 200, trim: true, required: true },
+    phone_number: { type: String, maxlength: 100 },
+    birth_day: { type: Date },
+    profile_image: { type: String, maxlength: 300 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Users = model<DBUser>("Users", UsersSchema);
