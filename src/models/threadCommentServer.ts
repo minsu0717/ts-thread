72bc0261a15a1540e5f9b7c2002071598839589e
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2024-04-26 20:47:21 +0900

    comment스키마 수정

--- src/models/threadCommentServer.ts
+++ src/models/threadCommentServer.ts
@@ -3,14 +3,14 @@ import { Schema, model, Types } from "mongoose";
 interface threadComments {
   thread_id: Types.ObjectId;
   user_id: Types.ObjectId;
-  content: string;
+  comment: string;
 }
 
 const threadCommentsSchema = new Schema<threadComments>(
   {
     thread_id: { type: Schema.Types.ObjectId, required: true, ref: "Threads" },
     user_id: { type: Schema.Types.ObjectId, required: true, ref: "Users" },
-    content: { type: String, maxlength: 500 },
+    comment: { type: String, maxlength: 500 },
   },
   {
     timestamps: true,
